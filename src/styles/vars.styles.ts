db862abf4c53d2cbf957f3cef5fc8300dce1785b
illuminarffx <fforzamax@gmail.com>
2025-06-10 10:01:34 +0300

    feat(styles): add ui-kit variables

---
+++ src/styles/vars.styles.ts
@@ -0,0 +1,46 @@
+export const variables = `
+  :root {
+    /* Typography */
+    --ttg-font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+    --ttg-font-weight-medium: 500;
+    --ttg-letter-spacing: -0.02em;
+    
+    /* Text sizes */
+    --ttg-text-size-m: 16px;
+    --ttg-text-line-height-m: 24px;
+    --ttg-text-size-s: 14px;
+    --ttg-text-line-height-s: 20px;
+    --ttg-text-size-xs: 12px;
+    --ttg-text-line-height-xs: 16px;
+    
+    /* Text colors */
+    --ttg-color-text-black: #333333;
+    --ttg-color-text-gray-900: #666666;
+    --ttg-color-text-gray-700: #888888;
+    --ttg-color-text-gray-500: #A9A9A9;
+    --ttg-color-text-gray-300: #D5D5D5;
+    --ttg-color-text-white: #FFFFFF;
+    --ttg-color-text-blue-base: #3290FF;
+    --ttg-color-text-critical: #D056CC;
+    
+    /* Background colors */
+    --ttg-color-bg-black: #333333;
+    --ttg-color-bg-gray-900: #666666;
+    --ttg-color-bg-gray-700: #888888;
+    --ttg-color-bg-gray-500: #A9A9A9;
+    --ttg-color-bg-gray-300: #D5D5D5;
+    --ttg-color-bg-gray-100: #F5F5F5;
+    --ttg-color-bg-white: #FFFFFF;
+    --ttg-color-bg-blue-base: #3290FF;
+    --ttg-color-bg-blue-dark: #137BEC;
+    --ttg-color-bg-blue-darker: #04602A;
+    --ttg-color-bg-blue-light: #073DFE;
+    --ttg-color-bg-blue-lighter: #E9F3FF;
+    
+    /* Stroke colors */
+    --ttg-color-stroke-black: #333333;
+    --ttg-color-stroke-gray-300: #D5D5D5;
+    --ttg-color-stroke-blue-base: #3290FF;
+    --ttg-color-stroke-critical: #D056CC;
+  }
+`;
