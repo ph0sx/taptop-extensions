bdda1eff3fb23ec5e8d2c69d524419e0b81ecddf
illuminarffx <fforzamax@gmail.com>
2025-07-29 20:49:08 +0300

    feat(animations): add lottie scroll sticky component

---
+++ src/entries/entry-lottie-scroll-generator.ts
@@ -0,0 +1,4 @@
+import '../utils/global-styles';
+import '../components/lottie/LottieScrollGenerator.js';
+
+export { default as LottieScrollGenerator } from '../components/lottie/LottieScrollGenerator.js';
---
+++ src/entries/entry-lottie-scroll-sticky-generator.ts
@@ -0,0 +1,4 @@
+import '../utils/global-styles';
+import '../components/lottie/LottieScrollStickyGenerator.js';
+
+export { default as LottieScrollStickyGenerator } from '../components/lottie/LottieScrollStickyGenerator.js';
